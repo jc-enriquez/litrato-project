3a956967955676007ab596fc67a9bce52c3516d2
unknown <john.christopher.enriquez2@gmail.com>
2023-03-04 23:42:37 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+  loop: true,
+  autoplay: true,
+  grabCursor: true,
+});
